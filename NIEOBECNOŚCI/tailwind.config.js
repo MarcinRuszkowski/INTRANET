d7dddfcffeef2c5de947690694af2c3197c01e12
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2024-07-31 09:11:06 +0200

    a lot of changes

---
+++ NIEOBECNOŚCI/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/**/*.{html,js}"],
+
+  // theme: {
+  //   extend: {
+  //     screens: {
+  //       sm: { min: max: "500px" },
+  //     },
+  //   },
+  // },
+  plugins: [],
+};
