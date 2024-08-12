fe14dae35b2e0aca0d5ecdcb60faf8d865841ff2
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2024-08-12 08:05:12 +0200

    poprawienie funkcji z zakładkami [działy.html]

--- public/js/działy.js
+++ public/js/działy.js
@@ -1,17 +1,19 @@
 function showDepartmentTab() {
   const departmentTabs = document.querySelectorAll(".department-tab");
 
-  departmentTabs.forEach((departmentTab) => {
-    departmentTab.addEventListener("click", () => {
+  departmentTabs.forEach((tab) => {
+    tab.addEventListener("click", (event) => {
+      event.preventDefault(); // Zapobiega domyślnemu zachowaniu linku (np. nawigacji)
+
       // Usuwanie klasy 'text-blue-700' ze wszystkich zakładek i dodanie 'text-gray-500'
-      departmentTabs.forEach((tab) => {
-        tab.classList.remove("text-blue-700");
-        tab.classList.add("text-gray-500");
+      departmentTabs.forEach((otherTab) => {
+        otherTab.classList.remove("text-blue-700");
+        otherTab.classList.add("text-gray-500");
       });
 
-      // Dodanie klasy 'text-blue-700' do klikniętej zakładki
-      departmentTab.classList.add("text-blue-700");
-      departmentTab.classList.remove("text-gray-500");
+      // dodanie klasy 'text-blue-700' do klikniętej zakładki
+      tab.classList.add("text-blue-700");
+      tab.classList.remove("text-gray-500");
     });
   });
 }
